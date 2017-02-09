7d04f9e8084e9c780ba0e7d9ed6acee3a276d083
WangShuoICT <1049133063@qq.com>
2017-02-09 16:02:40 +0800

    2016-2-9

---
+++ smith-waterman/smith-waterman/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  smith-waterman
+//
+//  Created by 王硕 on 2017/2/9.
+//  Copyright © 2017年 王硕. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
